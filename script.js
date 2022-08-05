6f7aa8dc6d9584dd25378bc07fe0b4eb8103c05e
sirinnicha <sirinnicha.ta@gmail.com>
2022-08-05 23:37:35 +0700

    add

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ const todoCtn = document.getElementById("todo-container");
 
 inputAdd.onkeyup = (event) => {
   if (event.key !== "Enter") return;
-
   //your code here
+  else if (inputAdd.value === "") {
+    alert("Please insert the title");
+  } else addTodo(inputAdd.value, false);
 };
 
 function addTodo(title, completed) {
@@ -30,19 +32,79 @@ function addTodo(title, completed) {
 
   //your code here
   //append todo to HTML...
+  const listTodo = document.getElementById("todo-container");
+  div.appendChild(span);
+  div.appendChild(doneBtn);
+  div.appendChild(deleteBtn);
+  listTodo.insertBefore(div, listTodo.children[0]);
+
+  document.getElementById("input-add-todo").value = "";
+
   //define buttons event...
+  doneBtn.onclick = function () {
+    doneTodo();
+    saveTodo();
+  };
+
+  function doneTodo() {
+    if (completed == 0) {
+      completed = 1;
+    } else if (completed == 1) completed = 0;
+
+    div.removeChild(span);
+    div.removeChild(deleteBtn);
+    div.removeChild(doneBtn);
+    div.appendChild(span);
+    span.style.textDecoration = completed ? "line-through" : "";
+    div.appendChild(doneBtn);
+    div.appendChild(deleteBtn);
+  }
+
+  deleteBtn.onclick = function () {
+    deleteTodo();
+    saveTodo();
+  };
+
+  function deleteTodo() {
+    div.remove();
+  }
+
+  saveTodo();
 }
 
 function saveTodo() {
   const data = [];
   for (const todoDiv of todoCtn.children) {
     //your code here
+    const todoObj = {};
+    todoObj.title = todoDiv.children[0].innerText;
+    todoObj.completed =
+      todoDiv.children[0].style.textDecoration === "line-through";
+    data.push(todoObj);
   }
   //your code here
+  if (JSON.parse(localStorage.getItem("index")) === null) {
+    const temp = [];
+    for (let i = 0; i < data.length; i++) {
+      temp.push(data[i]);
+    }
+    localStorage.setItem("index", JSON.stringify(temp));
+  } else if (JSON.parse(localStorage.getItem("index")) !== null) {
+    const temp = [];
+    for (let i = 0; i < data.length; i++) {
+      temp.push(data[i]);
+    }
+    localStorage.setItem("index", JSON.stringify(temp));
+  }
 }
 
 function loadTodo() {
   //your code here
+  const y = JSON.parse(localStorage.getItem("index"));
+
+  for (let i = y.length - 1; i >= 0; i--) {
+    addTodo(y[i].title, y[i].completed);
+  }
 }
 
 loadTodo();
